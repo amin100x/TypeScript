52196a479b25b5d511980a9ed5af899c7dcd9d6e
Mahmadamin Lodhiya <lodhiyaamin88@gmail.com>
2023-11-21 09:18:33 +0530

    ts basics

---
+++ Basics & Data Types/build/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let username = 'Amin';
+console.log(username);
+let a = 12;
+let b = '6';
+let c = 2;
+// will detect the error 
+console.log(a / b);
+// will detect the error 
+console.log(c * b);
---
+++ Basics & Data Types/src/main.ts
@@ -0,0 +1,14 @@
+let username = 'Amin'
+console.log(username)
+
+let a: number = 12
+let b: string = '6'
+let c: number = 2
+
+// will detect the error 
+console.log(a / b)
+
+// will detect the error 
+console.log(c * b)
+
+// Data Types
