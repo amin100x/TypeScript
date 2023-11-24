87cae2d08b4e1ef22e38b301ebf0dda74d4a8b0e
Mahmadamin Lodhiya <lodhiyaamin88@gmail.com>
2023-11-24 17:35:27 +0530

    advance classes

--- Class/build/js/class.js
+++ Class/build/js/class.js
@@ -28,3 +28,82 @@ class Coder2 {
         this.skills = skills;
     }
 }
+const dhoni = new Coder("MsDhoni", 42, "java, js, c++, rust");
+const dhoni1 = new Coder1("MsDhoni", 42, "java, js, c++, rust");
+const dhoni2 = new Coder2("MsDhoni", 42, "java, js, c++, rust");
+console.log(dhoni1.getAge());
+// inheritance
+class WebDev extends Coder {
+    constructor(name, age, skills, isBlockchainDev) {
+        super(name, age, skills); // we must first call the super method before writtng anything if we inherite and initialize all the vari of super class
+        this.isBlockchainDev = isBlockchainDev;
+        this.isBlockchainDev = isBlockchainDev;
+    }
+    getSkills() {
+        return this.skills;
+    }
+}
+const Smith = new WebDev("S.Smith", 35, "blochchain Dev", true);
+console.log(Smith.getSkills());
+class Bowler {
+    constructor(name, age, playerType) {
+        this.name = name;
+        this.age = age;
+        this.playerType = playerType;
+    }
+    play() {
+        return `${this.name} is ${this.playerType}`;
+    }
+}
+const Amin = new Bowler("Amin", 21, "Bowl");
+console.log(Amin.play());
+// ============== static use =======================
+class Peers {
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peers.count;
+    }
+    getCount() {
+        return Peers.count;
+    }
+}
+Peers.count = 0;
+const msd = new Peers("dhoni");
+const msd1 = new Peers("dhoni1");
+const msd2 = new Peers("dhoni2");
+const msd3 = new Peers("dhoni3");
+console.log(msd.id);
+console.log(msd1.id);
+console.log(msd2.id);
+console.log(Peers.count);
+// ================= getter Setter ==================
+class Bank {
+    constructor() {
+        this.AccNo = 0;
+        this.Tran = [];
+    }
+    get accNo() {
+        return this.AccNo;
+    }
+    set accNo(accno) {
+        this.AccNo = accno;
+        return;
+    }
+    get trann() {
+        return this.Tran;
+    }
+    set trann(tran) {
+        if (Array.isArray(tran) && tran.every((e) => typeof e === 'number')) {
+            this.Tran = tran;
+            return;
+        }
+        else
+            throw new Error("invalid parameter");
+    }
+}
+const mahi = new Bank();
+mahi.trann = [1000, 1500, 56656, 565];
+console.log(mahi.trann);
+mahi.trann = [...mahi.trann, 1000, 5000];
+console.log(mahi.trann);
--- Class/src/class.ts
+++ Class/src/class.ts
@@ -36,3 +36,109 @@ class Coder2 {
     }
 }
 
+const dhoni = new Coder("MsDhoni", 42, "java, js, c++, rust");
+const dhoni1 = new Coder1("MsDhoni", 42, "java, js, c++, rust");
+const dhoni2 = new Coder2("MsDhoni", 42, "java, js, c++, rust");
+console.log(dhoni1.getAge());
+
+
+// inheritance
+class WebDev extends Coder {
+
+    constructor(name: string, age: number, skills: string, public isBlockchainDev: boolean) {
+        super(name, age, skills) // we must first call the super method before writtng anything if we inherite and initialize all the vari of super class
+        this.isBlockchainDev = isBlockchainDev
+    }
+    getSkills() {
+        return this.skills
+    }
+}
+
+const Smith = new WebDev("S.Smith", 35, "blochchain Dev", true);
+console.log(Smith.getSkills());
+
+// ========================= interface implementation ====================
+
+interface Cricketer {
+    name: string
+    age: number
+    playerType: "Bowl" | "Bat" | "All"
+    play(action: string): string
+}
+
+class Bowler implements Cricketer {
+    name: string
+    age: number
+    playerType: "Bowl" | "Bat" | "All"
+    constructor(name: string, age: number, playerType: "Bowl" | "Bat" | "All") {
+        this.name = name
+        this.age = age
+        this.playerType = playerType
+    }
+    public play(): string {
+        return `${this.name} is ${this.playerType}`
+    }
+}
+const Amin = new Bowler("Amin", 21, "Bowl");
+console.log(Amin.play());
+
+// ============== static use =======================
+
+class Peers {
+    static count: number = 0
+    public id: number
+
+    constructor(public name: string) {
+        this.name = name
+        this.id = ++Peers.count
+    }
+    public getCount(): number {
+        return Peers.count
+    }
+}
+
+const msd = new Peers("dhoni")
+const msd1 = new Peers("dhoni1")
+const msd2 = new Peers("dhoni2")
+const msd3 = new Peers("dhoni3")
+
+console.log(msd.id);
+console.log(msd1.id);
+console.log(msd2.id);
+console.log(Peers.count);
+
+// ================= getter Setter ==================
+
+class Bank {
+    private AccNo: number
+    private Tran: number[]
+
+    constructor() {
+        this.AccNo = 0
+        this.Tran = []
+    }
+
+    public get accNo(): number {
+        return this.AccNo
+    }
+    public set accNo(accno: number) {
+        this.AccNo = accno
+        return
+    }
+    public get trann(): number[] {
+        return this.Tran
+    }
+    public set trann(tran: number[]) {
+        if (Array.isArray(tran) && tran.every((e) => typeof e === 'number')) {
+            this.Tran = tran
+            return
+        }
+        else throw new Error("invalid parameter")
+    }
+}
+
+const mahi = new Bank();
+mahi.trann = [1000, 1500, 56656, 565]
+console.log(mahi.trann);
+mahi.trann = [...mahi.trann, 1000, 5000]
+console.log(mahi.trann);
